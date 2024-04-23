72800bcb23c5646fd320b263f979f4b2e3e85216
Your Name <you@example.com>
2024-04-23 12:00:42 -0400

    COSAS

--- js/funciones.js
+++ js/funciones.js
@@ -2,7 +2,9 @@ function validar(){
     let nombre = document.getElementById("nombre").value;
     let rut = document.getElementById("rut").value;
     let correo = document.getElementById("correo").value;
-    
+    if(String(nombre).length>=15){
+        
+    }
 
 
 
